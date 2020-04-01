a787daeed7bf04ce356f0c8a917443e255350fd2
Mauro Hernandez <mauroDeveloper@hotmail.com>
2020-04-01 03:46:57 -0430

    deploy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('server work');
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));//indicar donde estan las vistas
+app.engine('html', require('ejs').renderFile)//motor de plantillas entienda html
+app.set('view engine', 'ejs');//motor de plantillas
+
+//middlewares
+
+
+//routes
+app.use(require('./routes/'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+//listening the serer
+app.listen(app.get('port'), ()=>{
+    console.log(`Server on port: ${app.get('port')}`);
+})
